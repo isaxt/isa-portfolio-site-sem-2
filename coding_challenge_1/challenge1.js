6111d5a5434c59cbeece869d404e50b24fc66d67
Isabella Tedesco <isaxt.tedesco@gmail.com>
2025-02-10 16:59:10 -0500

    coding challenge 2/10

---
+++ coding_challenge_1/challenge1.js
@@ -0,0 +1,26 @@
+let myFont;
+
+const ourWord = "MOO";
+
+let pointArray;
+
+function preload() {
+    myFont = loadFont("./comicSans.ttf");
+}
+
+function setup() {
+    createCanvas(400, 200).parent("my-sketch");
+    background(0, 0, 0);
+
+}
+
+function draw() {
+    pointArray = myFont.textToPoints(ourWord, 20, 150, 135, {sampleFactor: 0.2 });
+    
+     //1- solid green text
+     for (let i = 0; i < pointArray.length; i++) {
+
+        fill(255, 0, 0)
+        circle(pointArray[i].x, pointArray[i].y, 5);
+    }
+}
