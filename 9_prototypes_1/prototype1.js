29074c3310e52713f54740e874458078ddeda854
Isabella Tedesco <isaxt.tedesco@gmail.com>
2025-02-18 18:15:02 -0500

    2/18

--- 9_prototypes_1/prototype1.js
+++ 9_prototypes_1/prototype1.js
@@ -1,37 +1,47 @@
 let myFont;
-
 let pointArray;
 
-let r= 30;
+let r = 30;
 let angle = 0;
-let size = 300;
+let size = 200;
 let msg = "meep";
 
+
+// follows the mouse interaction
 function preload() {
-    myFont = loadFont("./GlitchGoblin-2O87v.ttf");
+
+    // loading the font
+    myFont = loadFont("GlitchGoblin-2O87v.ttf");
 }
 
 function setup() {
-
-    // play with the sizing
-    createCanvas(400, 400).parent("my-sketch");
+    createCanvas(600, 580);
+    angleMode(DEGREES);
     
-    pointArray = font.textToPoints(msg, -0, 300, size, { sampleFactor: 0.1, simplifyThreshold: 0 });
-          angleMode(DEGREES);
-    
-    
-
+    //font is loaded before calling textToPoints--> sometimes it wouldn't load and I was so frustrated
+    if (myFont) {
+        pointArray = myFont.textToPoints(msg, 0, 10, size, { sampleFactor: 0.1, simplifyThreshold: 0 });
+         } else {
+        console.error("Font failed to load.");
+        pointArray = [];
+    }
 }
 
 function draw() {
     background(188, 235, 207);
+    
+    //mouseX--> horizontal position of the mouse within the canvas
+    //angleMode in setup--> use angles 
+    let x = r * cos(map(mouseX, 0, width, 0, 360)); 
+    let y = r * sin(map(mouseY, 0, height, 0, 360));
 
-    let x = r*cos(map(mouseX, 0, width, 0, 360)); 
-    let y = r*sin(map(mouseY, 0, height, 0, 360));
-
-    translate(20,300);
-    for (let i = 0; i < points.length; i++) {
-        line(points[i].x, points[i].y, points[i].x+x, points[i].y + y);
-}
-
+    translate(40, 300);
+    
+    // set stroke color to be more clear
+    stroke(0); 
+    strokeWeight(1.5); // increase/decrease the width
+    
+    for (let i = 0; i < pointArray.length; i++) {
+        line(pointArray[i].x, pointArray[i].y, pointArray[i].x + x, pointArray[i].y + y);
+    }
 }
